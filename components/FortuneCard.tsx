cb9c6f8e5cf28cb123a625b4793ff93676abcee3
Fintegrate-Community <contact@fintegrate.co.uk>
2026-09-03 22:15:22 +0100

    Add PersonalityCard to FortuneCard component

--- components/FortuneCard.tsx
+++ components/FortuneCard.tsx
@@ -1,6 +1,7 @@
 import type { MarketEvent } from "@/lib/events";
 import ArcanaCard from "@/components/ArcanaCard";
 import EventCard from "@/components/EventCard";
+import PersonalityCard from "@/components/PersonalityCard";
 
 export default function FortuneCard({
   event,
@@ -12,9 +13,10 @@ export default function FortuneCard({
   daysAway: number;
 }) {
   return (
-    <div className="mx-auto grid w-full max-w-5xl grid-cols-1 gap-8 sm:grid-cols-2">
+    <div className="mx-auto grid w-full max-w-7xl grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
       <ArcanaCard arcanaId={event.arcana} />
       <EventCard event={event} exact={exact} daysAway={daysAway} />
+      <PersonalityCard />
     </div>
   );
 }
